fc7a1f3e107c3cf1a592b38fd94ceaa8518df4fb
phantom22222 <sakshamkapatia@gmail.com>
2021-06-22 20:38:03 +0530

    add platform1

--- sketch.js
+++ sketch.js
@@ -35,6 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    platform1 = new Ground(100,300,120,170);
 
 }
 
@@ -61,4 +62,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    platform1.display();
 }
